0433c0a5f21b4bd8292b2c91f509ba02cc938df6
annetpault <annetmca25@gmail.com>
2026-07-14 15:29:18 +0530

    Nav Bar completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AddLog from './components/AddLog'
+import ViewLog from './components/ViewLog'
 
 
 function App() {
@@ -10,7 +13,13 @@ function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+
+        <Route path='/' element={<AddLog />} />
+        <Route path='/view' element={<ViewLog />} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
